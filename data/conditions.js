45a91e6aec07a9255ab91d6649fe505d30d27d52
Guido Kessels <guidokessels@gmail.com>
2016-11-06 13:13:15 +0100

    New: Condition cards
    - Fanatical Devotion
    - I'll Show You the Dark Side
    - Suppressive Fire

---
+++ data/conditions.js
@@ -0,0 +1,26 @@
+[
+  {
+    "image": "conditions/ill-show-you-the-dark-side.png",
+    "name": "I'll Show You the Dark Side",
+    "xws": "illshowyouthedarkside",
+    "text": "When this card is assigned, if it is not already in play, the player who assigned it searches the Damage deck for 1 Damage card with the <strong>Pilot</strong> trait and may place it faceup on this card. Then shuffle the damage deck.<br /><br />When you suffer critical damage during an attack, you are instead dealt the chose faceup Damage card.<br /><br />When there is no Damage card on this card, remove it.",
+    "unique": true,
+    "id": 0
+  },
+  {
+    "image": "conditions/fanatical-devotion.png",
+    "text": "When defending, you cannot spend focus tokens.<br /><br />When attacking, if you spend a focus token to change all [Focus] results to [Hit] results, set aside the first [Focus] result that you change. The set-aside [Hit] result cannot be canceled by defense dice, but the defender may cancel [Critical Hit] results before it.<br /><br />During the End phase, remove this card.",
+    "name": "Fanatical Devotion",
+    "xws": "fanaticaldevotion",
+    "unique": true,
+    "id": 1
+  },
+  {
+    "image": "conditions/suppressive-fire.png",
+    "text": "When attacking a ship other than \"Captain Rex,\" roll 1 fewer attack die.<br /><br />When you declare an attack targeting \"Captain Rex\" or when \"Captain Rex\" is destroyed, remove this card.<br /><br />At the end of the Combat phase, if \"Captain Rex\" did not perform an attack this phase, remove this card.",
+    "name": "Suppressive Fire",
+    "xws": "suppressivefire",
+    "unique": true,
+    "id": 2
+  }
+]
