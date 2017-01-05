6b146087f3f09d933ed3514279848f8f13a164d4
Luis Visintini <lvisintini@gmail.com>
2017-01-05 16:07:15 -0300

    Data integrity tests

---
+++ tests/utils.js
@@ -0,0 +1,45 @@
+var assert = require("assert");
+
+
+var DATASET_HEADER_MAP = {
+  conditions: "Condition",
+  damageDeckCore: "DamageDeckCore",
+  damageDeckCoreTfa: "DamageDeckCore",
+  pilots: "Pilot",
+  ships: "Ship",
+  sources: "Source",
+  upgrades: "Upgrade",
+};
+
+function buildDataHeader(data, dataKey ,index) {
+  var dataHeader;
+  var header = DATASET_HEADER_MAP[dataKey];
+  var model = data[dataKey][index];
+
+  if (typeof model.id !== "undefined" && model.name) {
+    dataHeader = "\t<" + header + " id=" + model.id + " name=\"" + model.name + "\">";
+  } else if (typeof model.id !== "undefined") {
+    dataHeader = "\t<" + header + " id=" + model.id + ">";
+  } else if (model.name) {
+    dataHeader = "\t<" + header + " name=\"" + model.name + "\">";
+  } else {
+    dataHeader = "\t<" + header + " index=" + index + ">";
+  }
+
+  return dataHeader
+}
+
+function outputAllErrors(errors) {
+  if (errors.length > 0) {
+    assert.fail(
+      errors.length + " Validation Errors",
+      "0 Validation Errors" ,
+      "\n" + errors.join("\n")
+    );
+  }
+}
+
+module.exports = {
+  buildDataHeader: buildDataHeader,
+  outputAllErrors: outputAllErrors,
+};
